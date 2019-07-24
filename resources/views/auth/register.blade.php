7e868b1342882201e05e34bcecc85b6b98dc66d8
Dewi Ayu Nirmalasari <dnirmalasari18@gmail.com>
2019-07-24 14:36:56 +0700

    production step

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -75,10 +75,10 @@
     </div>
 
 
-    <script src="{!!asset('template/vendors/jquery/dist/jquery.min.js')!!}"></script>
-    <script src="{!!asset('template/vendors/popper.js/dist/umd/popper.min.js')!!}"></script>
-    <script src="{!!asset('template/vendors/bootstrap/dist/js/bootstrap.min.js')!!}"></script>
-    <script src="{!!asset('template/assets/js/main.js')!!}"></script>
+    <script src="{!!secure_asset('template/vendors/jquery/dist/jquery.min.js')!!}"></script>
+    <script src="{!!secure_asset('template/vendors/popper.js/dist/umd/popper.min.js')!!}"></script>
+    <script src="{!!secure_asset('template/vendors/bootstrap/dist/js/bootstrap.min.js')!!}"></script>
+    <script src="{!!secure_asset('template/assets/js/main.js')!!}"></script>
 
 
 </body>
