4518e94adf652394f61e0e51d59f6a57ae4b1f41
odeloen <odeloen.nakmus@gmail.com>
2019-07-26 13:26:33 +0700

    fixing middleware

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('logout', function(){
 Route::get('reset','UserController@reset')->name('reset');
 Route::post('reset','UserController@doReset')->name('password.update');
 
-Route::prefix('corps/')->group(['middleware' => 'koordinator'],function(){
+Route::prefix('corps/')->group(['middleware' => ['koordinator']], function(){
     Route::get('{id}','CorpController@show')->name('corp.show');
     Route::post('note/create','CorpController@noteStore')->name('corp.note.create');
     Route::post('note/delete','CorpController@noteDelete')->name('corp.note.delete');
